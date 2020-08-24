a02c40efdbc0e0d5c78cf8e43447f710a5273ef0
shafiqrefat <shafiqrefat@gmail.com>
2020-08-25 02:49:35 +0600

    shop page coding done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,15 +4,38 @@ const Cart = (props) => {
     const cart = props.cart;
     // const total = cart.reduce( (total,products) => total +products.price , 0 )
     let total = 0;
-    for(let i = 0; i<cart.length; i++){
+    for (let i = 0; i < cart.length; i++) {
         const product = cart[i];
         total = total + product.price;
     }
+
+    let shipping = 0;
+    if (total > 35) {
+        shipping = 0;
+    }
+    else if (total > 15) {
+        shipping = 4.99;
+    }
+    else if (total > 0) {
+        shipping = 12.99;
+    }
+
+    const tax = (total / 10).toFixed(2);
+    const grandTotal = (total + shipping + Number(tax)).toFixed(2);
+
+    const formatNumber = num => {
+        const precision = num.toFixed(2);
+        return Number(precision); 
+    }
     return (
         <div>
-            <h4>Order Summery</h4>
+            <h4>Order Summary</h4>
             <p>Items Ordered: {cart.length}</p>
-            <p>Total price {total}</p>
+            <p>Product Price: {formatNumber(total)}</p>
+            <p><small>Shipping Cost: {shipping}</small></p>
+            <p><small>Tax + VAT: {tax}</small></p>
+            <p><span>Total Price {grandTotal}</span></p>
+            <button className="main-button">Review your order</button>
         </div>
     );
 };
