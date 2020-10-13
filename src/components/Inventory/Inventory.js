ff2770b4cf3f8f663ecc9c0ed03be87fd7b1782c
shafiqrefat <shafiqrefat@gmail.com>
2020-10-13 23:53:10 +0600

    heroku added

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Inventory = () => {
     const handleAddProduct = () => {
         const product = {};
-        fetch('http://localhost:5000/addProduct', {
+        fetch('https://fast-headland-22715.herokuapp.com/addProduct', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
--- src/components/ProductDetail/ProductDetail.js
+++ src/components/ProductDetail/ProductDetail.js
@@ -9,7 +9,7 @@ const ProductDetail = () => {
     const [product,setProduct] = useState({});
 
     useEffect(() => {
-        fetch('http://localhost:5000/product/' + productKey)
+        fetch('https://fast-headland-22715.herokuapp.com/product/' + productKey)
         .then(response => response.json())
         .then(data => setProduct(data))
     },[productKey])
